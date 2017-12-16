e440a7147571dc1d4b9ad92712688a9cf76de384
Eduard Litau <eduard.litau@gmail.com>
2017-12-17 00:32:49 +0100

    More inline docs

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # This script sets required options, starts mongodb and wekan
 
-export MONGO_URL="mongodb://localhost:27017"
+# Wekan can afterwards be requested at PORT
 export PORT=60835
+# Used eg. for the links in emails
 export ROOT_URL="http://localhost:$PORT"
+# Wekan uses MONGO_URL to connect to mongodb
+export MONGO_URL="mongodb://localhost:27017"
 
 ## Start mongodb
 mongod --dbpath $HOME/wekan/data/db &
